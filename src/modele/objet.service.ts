e76e1607a4cc4c222b3456c008b57498c4581f05
cid <ciditworld@gmail.com>
2019-10-17 13:28:03 -0400

    stuff
    stuff

---
+++ src/modele/objet.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
+import { throwError, Observable } from 'rxjs';
+
+import { retry, catchError } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: "root"
+})
+export class ExerciseObject{
+
+
+    baseUrl = 'http://localhost:3000';
+
+    constructor(private http: HttpClient){
+
+    }
+
+
+    //formatage http Headers
+
+    httpOpions={
+        headers : new HttpHeaders({
+            'Content-Type': 'application/json'
+        })
+    }
+
+    //methode GET
+
+    GetStudent(): Observable<Exercice>{
+        return this.http.get<Exercice>(this.baseUrl +'/Exercise')
+        .pipe(
+            retry(1), catchError(this.errorHandl)
+        )
+    }
+
+    errorHandl(error){
+
+        let errorMessage = '';
+        if(error.error instanceof ErrorEvent){
+            errorMessage = error.error.message;
+        }
+
+        console.log(errorMessage);
+        return throwError(errorMessage);
+    }
+}
