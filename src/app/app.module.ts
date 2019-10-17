ce6fc7a0a37e2f7bc935f68195284f07f64dc85b
cid <ciditworld@gmail.com>
2019-10-17 14:38:44 -0400

    stuff again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListExerciceComponent } from './list-exercice/list-exercice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListExerciceComponent
   ],
   imports: [
     BrowserModule,
