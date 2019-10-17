4f56f55bcb3841ae6c417537cccc29ed0f906d5f
bluesylveon <44065730+bluesylveon@users.noreply.github.com>
2019-10-17 14:53:56 -0400

    question2

---
+++ question2.ts
@@ -0,0 +1,60 @@
+
+class Objet1 {
+    x: number;
+}
+//One to one + Agregation
+class OneToOneAgregation {
+    obj: Objet1;
+}
+
+//One to many 
+class OneToMany {
+    obj: Objet1[];
+}
+
+//Many to many
+class ManyToMany {
+    objet2: OneToOneAgregation;
+    objet1: Objet1;
+}
+
+// Heritage (Generalisation)
+class Parent {
+    x: number;
+}
+class Enfant extends Parent {
+    y: number;
+}
+
+// Dependance
+class Dependance {
+    y: number;
+    obj: Objet1;
+
+    methode() {
+        return this.y + this.obj.x;
+    }
+}
+
+// Realisation
+interface Inter {
+    g: number;
+}
+class Realisation implements Inter {
+    g: number;
+}
+
+//Composition 
+class Composition1 {
+    x: number;
+    getX() { return this.x; }
+    setX(x: number) { this.x = x; }
+}
+class Composition2 {
+    obj: Composition1;
+    y: number;
+    getY() { return this.y; }
+    setY(y: number) { this.y = y; }
+}
+
+//Question 3
