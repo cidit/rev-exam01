fcd81ce0904c1beaa7f4b72179185f7de1cd2270
cid <ciditworld@gmail.com>
2019-10-17 14:12:05 -0400

    push numero cinco

--- src/modele/objet.service.ts
+++ src/modele/objet.service.ts
@@ -7,52 +7,52 @@ import { retry, catchError } from 'rxjs/operators';
 @Injectable({
     providedIn: "root"
 })
-export class ExerciseObject {
+export class ExerciseObject{
 
     baseUrl = 'http://localhost:3000';
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {}
 
     //formatage http Headers
     httpOpions = {
-        headers: new HttpHeaders({
+        headers : new HttpHeaders({
             'Content-Type': 'application/json'
         })
     }
 
     //methode GET
-    GetStudent(): Observable<Exercice> {
-        return this.http.get<Exercice>(this.baseUrl + '/Exercise')
-            .pipe(
-                retry(1), catchError(this.errorHandl)
-            )
+    GetStudent(): Observable<Exercice>{
+        return this.http.get<Exercice>(this.baseUrl +'/Exercice')
+        .pipe(
+            retry(1), catchError(this.errorHandl)
+        )
     }
 
-    //methode PUT
-    PutStudent(): Observable<Exercice> {
-        return this.http.put<Exercice>(this.baseUrl + '/Exercise', '')
-            .pipe(
-                retry(1), catchError(this.errorHandl)
-            )
-    }
-    //methode POST
-    PostStudent(): Observable<Exercice> {
-        return this.http.post<Exercice>(this.baseUrl + '/Exercise', '')
-            .pipe(
-                retry(1), catchError(this.errorHandl)
-            )
-    }
-    //methode DELETE
-    DeleteStudent(): Observable<Exercice> {
-        return this.http.delete<Exercice>(this.baseUrl + '/Exercise')
-            .pipe(
-                retry(1), catchError(this.errorHandl)
-            )
+    //
+    PutStudent(body): Observable<Exercice>{
+        return this.http.put<Exercice>(this.baseUrl +'/Exercice', body)
+        .pipe(
+            retry(1), catchError(this.errorHandl)
+        )
     }
 
-    errorHandl(error) {
+    PostStudent(body): Observable<Exercice>{
+        return this.http.post<Exercice>(this.baseUrl +'/Exercice', body)
+        .pipe(
+            retry(1), catchError(this.errorHandl)
+        )
+    }
+
+    DeleteStudent(id): Observable<Exercice>{
+        return this.http.delete<Exercice>(this.baseUrl +'/Exercice/' + id)
+        .pipe(
+            retry(1), catchError(this.errorHandl)
+        )
+    }
+
+    errorHandl(error){
         let errorMessage = '';
-        if (error.error instanceof ErrorEvent) {
+        if(error.error instanceof ErrorEvent){
             errorMessage = error.error.message;
         }
 
